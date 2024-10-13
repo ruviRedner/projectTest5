7657c79fe2df8ae1202e3019e368e024404dfdcf
ruviRedner <r.y.r7966@gmail.com>
2024-10-13 15:07:38 +0300

    swagger file done

---
+++ src/config/swagger.ts
@@ -0,0 +1,19 @@
+import swaggerJsdoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Schol',
+      version: '1.0.0',
+      description: 'A swagger for schol ',
+    },
+  },
+  
+  apis: ['C:/Users/רובי/Desktop/projectTest5/dist/routes/*.js'], // Path to your API routes
+};
+
+ export const specs = swaggerJsdoc(options);
+
+ export default swaggerUi
