747598b786b3bf1e7bde0b101655b165f826f9ed
ruviRedner <r.y.r7966@gmail.com>
2024-10-13 17:57:08 +0300

    fix types

--- src/middleware/verifidUser.ts
+++ src/middleware/verifidUser.ts
@@ -1,55 +1,97 @@
+
 import { NextFunction, Request, Response } from "express";
 import jwt, { TokenExpiredError } from "jsonwebtoken";
 import RequestWithUser from "../interfaces/requestWithUser";
 import TokenPayloadDTO from "../interfaces/tokenPayloed";
 
+// const verifyUser = async (
+//   req: RequestWithUser | Request,
+//   res: Response,
+//   next: NextFunction
+// )  => {
+  
+//     try {
+//       const token: string  = req.cookies.login_token;
+
+//       if (!token) {
+//         return res.status(401).json({
+//           err: true,
+//           message: "Token is missing, please log in again",
+//           data: null,
+//         });
+//       }
+
+//       const payload: TokenPayloadDTO = jwt.verify(
+//         token,
+//         process.env.TOKEN_SECRET as string
+//       ) as TokenPayloadDTO;
+
+//       (req as RequestWithUser).user = payload;
+
+//       next();
+//     } catch (err) {
+//       if (err instanceof TokenExpiredError) {
+//         return res.status(401).json({
+//           err: true,
+//           message: "Token expired, please log in again",
+//           data: err,
+//         });
+//       }
+
+//       return res.status(400).json({
+//         err: true,
+//         message: "Token is missing or corrupted",
+//         data: err,
+//       });
+//     }
+// }
+
+
+// export default verifyUser;
+
+
 const verifyUser = async (
-  roll: string,
-  req: RequestWithUser | Request,
+  req: RequestWithUser |Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const token: string = req.cookies.login_token;
+    const token = req.cookies?.login_token;
+
     if (!token) {
-      res.status(401).json({
+      return res.status(401).json({
         err: true,
         message: "Token is missing, please log in again",
         data: null,
       });
-      return;
     }
 
-    console.log(token);
-    const payload: TokenPayloadDTO = jwt.verify(
+    const payload = jwt.verify(
       token,
       process.env.TOKEN_SECRET as string
     ) as TokenPayloadDTO;
+
     (req as RequestWithUser).user = payload;
-    if (payload.roll !== roll) {
-      res.status(403).json({
-        err: true,
-        message: "You are not authorized to access this resource",
-        data: null,
-      });
-      return;
-    }
+
     next();
   } catch (err) {
     if (err instanceof TokenExpiredError) {
-      res.status(401).json({
+      return res.status(401).json({
         err: true,
-        message: "Token expiered, please log in again",
-        data: err,
-      });
-    } else {
-      res.status(400).json({
-        err: true,
-        message: "Token is missing or curropted",
+        message: "Token expired, please log in again",
         data: err,
       });
     }
+
+    return res.status(400).json({
+      err: true,
+      message: "Token is invalid or corrupted",
+      data: err,
+    });
   }
 };
 
 export default verifyUser;
+
+
+
