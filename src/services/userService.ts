9d93a06a81f0fa741f46532fabb033e825531440
ruviRedner <r.y.r7966@gmail.com>
2024-10-13 17:12:45 +0300

    founction get student done

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import userModel from "../models/teacherOrStudentModel";
 import { IteacherOrStudent } from "../interfaces/IteacherOrStudent";
 import classModel from "../models/ClassModel";
+import gradsModel from "../models/gradsModel";
 
 export const CreateNewUser = async (newUser: IteacherOrStudent) => {
   try {
@@ -42,6 +43,37 @@ export const CreateClass = async (classs: string) => {
     throw err;
     }
 }
+
+export const GetstudentDetailsAmdGrads = async (Tid:string,StudentId:string) => {
+    try {
+    const student = await userModel.findById(StudentId)
+    if (!student) {
+        throw new Error("Student not found.");
+    }
+    const teacher = await userModel.findById(Tid)
+    if (!teacher) {
+        throw new Error("Teacher not found.");
+    }
+    const grads = await gradsModel.findOne({ teacherId: Tid, studentId: StudentId}).populate("grads");
+    if (!grads) {
+        throw new Error("Grades not found.");
+    }
+    return {
+        student,
+        teacher,
+        grads,
+    };
+}catch (err) {
+    console.error("Error getting student details and grades:", err);
+    throw err;
+    
+}
+}
+
+export const AddGrad = async (Tid:string, StudentId:string, grad: number) => {
+
+}
+
 
 
 
