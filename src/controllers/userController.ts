e37e15fa1dcc6a6c10ef8e26ee605e0e36c89713
ruviRedner <r.y.r7966@gmail.com>
2024-10-13 16:37:48 +0300

    add create class in controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from "express";
-import { CreateNewUser } from "../services/userService";
+import { CreateClass, CreateNewUser } from "../services/userService";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
     const newUser = await CreateNewUser(req.body);
-    res.status(201).json(newUser);
+    res.status(201).json({newUser});
   } catch (err) {
     res.status(500).json({ err: err });
   }
 };
+
+export const createNewClass = async (req: Request, res: Response) => {
+    try {
+      const newClass = await CreateClass(req.body);
+      res.status(201).json({ newClass });
+    } catch (err) {
+      res.status(500).json({ err: err });
+    }
+  };
 
 export const getStudent = async (req: Request, res: Response) => {};
