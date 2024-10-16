f843cf5adcdc7e7e60a853d981216ba3eebcd83b
ruviRedner <r.y.r7966@gmail.com>
2024-10-16 13:16:14 +0300

    update class model

--- src/interfaces/Iclass.ts
+++ src/interfaces/Iclass.ts
@@ -1,7 +1,8 @@
 import { Types } from 'mongoose';
+import { Itests } from './Igrads';
 
 export interface Iclass extends Document {
   name: string;
   teacherId: Types.ObjectId;
-  studentsId: Types.ObjectId[];
+  tests:Itests[];
 }
