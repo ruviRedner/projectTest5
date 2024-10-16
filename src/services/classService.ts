067fb98c8d1e3a848e34abd3a37ba8b265cd1fb8
ruviRedner <r.y.r7966@gmail.com>
2024-10-16 13:18:45 +0300

    new

---
+++ src/services/classService.ts
@@ -0,0 +1,37 @@
+
+
+// export const getAvarage = async (Tid: string): Promise<Number> => {
+//   const grades = await gradsModel.find({ teacherId: Tid });
+//   if (!grades) {
+//     throw new Error('No grades found for this teacher');
+//   }
+//   const ave = await gradsModel.aggregate([
+//     {
+//       $match: { teacherId: Tid }
+//     },
+//     {
+//       $group: {
+//         _id: '$subject',
+//         average: { $avg: '$average' }
+//       }
+//     }
+//   ]);
+//   return ave[0].average;
+// };
+// export const getStudentGradeByStudent = async (Sid: string) => {
+//   const grades = await gradsModel.find({ studentId: Sid });
+//   if (!grades) {
+//     throw new Error('No grades found for this student');
+//   }
+//   return grades;
+// };
+// export const getStudentGradeByIsTeacher = async (Tid: string) => {
+//   const grades = await gradsModel.find({ studentId: Tid });
+//   if (!grades) {
+//     throw new Error('No grades found for this student');
+//   }
+//   return grades;
+// };
+// export const updataGrade = async (Tid: string) => {
+//   const grades = await gradsModel.findByIdAndUpdate(Tid);
+// };
