5dad4d8da41040792f40c530ea069f1ba3b84ca8
ruviRedner <r.y.r7966@gmail.com>
2024-10-16 13:20:43 +0300

    delete notes

--- src/middleware/verifidUser.ts
+++ src/middleware/verifidUser.ts
@@ -3,49 +3,7 @@ import jwt, { TokenExpiredError } from 'jsonwebtoken';
 import RequestWithUser from '../interfaces/requestWithUser';
 import TokenPayloadDTO from '../interfaces/tokenPayloed';
 
-// const verifyUser = async (
-//   req: RequestWithUser | Request,
-//   res: Response,
-//   next: NextFunction
-// )  => {
 
-//     try {
-//       const token: string  = req.cookies.login_token;
-
-//       if (!token) {
-//         return res.status(401).json({
-//           err: true,
-//           message: "Token is missing, please log in again",
-//           data: null,
-//         });
-//       }
-
-//       const payload: TokenPayloadDTO = jwt.verify(
-//         token,
-//         process.env.TOKEN_SECRET as string
-//       ) as TokenPayloadDTO;
-
-//       (req as RequestWithUser).user = payload;
-
-//       next();
-//     } catch (err) {
-//       if (err instanceof TokenExpiredError) {
-//         return res.status(401).json({
-//           err: true,
-//           message: "Token expired, please log in again",
-//           data: err,
-//         });
-//       }
-
-//       return res.status(400).json({
-//         err: true,
-//         message: "Token is missing or corrupted",
-//         data: err,
-//       });
-//     }
-// }
-
-// export default verifyUser;
 
 const verifyUser = async (
   req: RequestWithUser | Request,
