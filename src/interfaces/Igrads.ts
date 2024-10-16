7110c87e258dc59b9f24ea5afba882e026b25150
ruviRedner <r.y.r7966@gmail.com>
2024-10-16 13:16:53 +0300

    create schema for tests

--- src/interfaces/Igrads.ts
+++ src/interfaces/Igrads.ts
@@ -1,15 +1,23 @@
-import { Types } from 'mongoose';
+// import { Types } from 'mongoose';
 
-export interface Igrads extends Document {
-  teacherId: Types.ObjectId;
+// export interface Igrads extends Document {
+//   teacherId: Types.ObjectId;
 
-  classId: Types.ObjectId;
-  greads: [
-    {
-      subject: string;
-      grade: number;
-      studentId: Types.ObjectId;
-    }
-  ];
-  average: number;
-}
+//   classId: Types.ObjectId;
+//   greads: [
+//     {
+//       subject: string;
+//       grade: number;
+//       studentId: Types.ObjectId;
+//     }
+//   ];
+//   average: number;
+// }
+import { Document, Schema, Types } from "mongoose";
+
+export interface Itests extends Document {
+    subject: string;
+    studentId: Types.ObjectId | string;
+    score: number
+  }
+ export const testSchema:Schema<Itests> = new Schema<Itests>({})
