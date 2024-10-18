6ff628676405dc63eee3cab0a4066c3255407277
ruviRedner <r.y.r7966@gmail.com>
2024-10-18 14:23:00 +0300

    connct to service

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,35 +1,33 @@
 import { Request, Response } from 'express';
-import {
-  CreateClass,
-  CreateNewUser,
-  GetstudentDetailsOfAllStudents
-} from '../services/userService';
+
 import RequestWithUser from '../interfaces/requestWithUser';
+import { createNewUser, getOneStudent, GetstudentDetailsOfAllStudents } from '../services/userService';
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const newUser = await CreateNewUser(req.body);
+    const newUser = await createNewUser(req.body);
     res.status(201).json({ newUser });
   } catch (err) {
-    res.status(500).json({ err: "gui" });
+    res.status(500).json({ err });
   }
 };
 
-export const creatNewClass = async (req: RequestWithUser, res: Response) => {
+
+export const getStudents = async (req: RequestWithUser, res: Response) => {
   try {
-    if (req.user.roll !== 'teacher') {
-      return res.status(403).json({ message: 'You are not a teacher' });
+    const student = await GetstudentDetailsOfAllStudents(req.user.id as any);
+    if (!student) {
+      return res.status(404).json({ message: 'Student not found' });
     }
-    const newClass = await CreateClass(req.body);
-    res.status(201).json({ newClass });
+    res.status(200).json({ student });
   } catch (err) {
     res.status(500).json({ err: err });
   }
 };
 
-export const getStudent = async (req: RequestWithUser, res: Response) => {
+export const getStudentOne = async (req: RequestWithUser, res: Response) => {
   try {
-    const student = await GetstudentDetailsOfAllStudents(req.user.id as any);
+    const student = await getOneStudent(req.user.id as any,req.params.id);
     if (!student) {
       return res.status(404).json({ message: 'Student not found' });
     }
