795c225a6e10df5bfbf7a95dd46ec0253319d0b6
ruviRedner <r.y.r7966@gmail.com>
2024-10-18 11:31:12 +0300

    founction to create teacher or student done

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,69 +1,47 @@
-import bcrypt from 'bcrypt';
-import userModel from '../models/teacherOrStudentModel';
+import bcrypt from "bcrypt"
 import { IteacherOrStudent } from '../interfaces/IteacherOrStudent';
-import classModel from '../models/ClassModel';
+import userModel from '../models/teacherOrStudentModel';
+import classModel from "../models/ClassModel";
+import { Iclass } from "../interfaces/Iclass";
 
-
-export const CreateNewUser = async (newUser: IteacherOrStudent) => {
+export const createUser = async (
+  newUser: IteacherOrStudent
+): Promise<void> => {
   try {
-    const { username, email, roll, password } = newUser;
-
-    // Validate input fields
-    if (!username || !email || !password) {
-      throw new Error('Missing required fields: username, email, or password.');
+    const { username, password, roll, email,classId } = newUser;
+    if(!username || !password || !roll || !email){
+      throw new Error("fileds are required")
     }
-    console.log(password);
-    
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
-    console.log("gygy");
-    
-    
-
-    const dbUser = new userModel({
+    const hashPassword = await bcrypt.hash(password,10)
+    if(!hashPassword){
+      throw new Error("bcrypt are faild")
+    }
+    const newuser = await new userModel({
       username,
-      password:hashedPassword,
-      email,
-      roll
-    });
-    console.log(dbUser);
-    await dbUser.save();
-    return dbUser;
-  } catch (err) {
-    console.error('Error creating user:', err);
-    throw err;
-  }
-};
-
-export const CreateClass = async (classs: string) => {
-  try {
-    const dbClass = new classModel({
-      name: classs
-    });
-    await dbClass.save();
-    return dbClass;
-  } catch (err) {
-    console.error('Error getting class:', err);
-    throw err;
-  }
-};
-
-export const GetstudentDetailsOfAllStudents = async (Tid: string) => {
-  try {
-    const teacher = await userModel.findById(Tid);
-    if (!teacher) {
-      throw new Error('Teacher not found.');
+      password:hashPassword,
+      roll,
+      email
+    })
+    if(newUser.roll === "teacher"){
+      const newClass = new classModel({
+        name:"english"
+      })
+      await newClass.save()
+      newuser.classId = newClass._id
     }
-    const students = await userModel.find({ roll: 'student' });
-    return students;
-  } catch (err) {
-    console.error('Error getting student details and grades:', err);
-    throw err;
+    if(newUser.roll === "student"){
+      await getClasses()
+      newUser.classId = classId
+    }
+    await newuser.save()
+    
+  } catch (error) {
+    throw new Error("bad request")
   }
 };
+const getClasses = async () :Promise<Iclass[]> => {
+  const clases:Iclass[] = await classModel.find({})
+  return clases
+}
 
-export const AddGrad = async (
-  Tid: string,
-  StudentId: string,
-  grad: number
-) => {};
+
