a68df93379acc310cc780de4fd33b195fe5320de
ruviRedner <r.y.r7966@gmail.com>
2024-10-18 14:22:36 +0300

    add id

--- src/interfaces/Iclass.ts
+++ src/interfaces/Iclass.ts
@@ -2,6 +2,7 @@ import { Types } from 'mongoose';
 import { Itests } from './Igrads';
 
 export interface Iclass extends Document {
+  _id : Types.ObjectId
   name: string;
   teacherId: Types.ObjectId;
   tests:Itests[];
