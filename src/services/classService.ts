72695070d47efe110ed643d020f2b20670df8a32
ruviRedner <r.y.r7966@gmail.com>
2024-10-18 16:02:11 +0300

    founction add

--- src/services/classService.ts
+++ src/services/classService.ts
@@ -1,37 +1,157 @@
+import mongoose from 'mongoose';
+import userModel from '../models/teacherOrStudentModel';
+import classModel from '../models/ClassModel';
 
+export const addScoreToStudent = async (
+  Tid: mongoose.Types.ObjectId,
+  newScore: number,
+  Sid: string,
+  subject: string
+): Promise<void> => {
+  //find teacher
+  const findTeacher = await userModel.findById(Tid);
+  if (!findTeacher || findTeacher.roll !== 'teacher') {
+    throw new Error('you are not a teacher');
+  }
+  const findClass = await classModel.findById(Tid);
+  if (!findClass) {
+    throw new Error('no class found');
+  }
+  const findStudent = await userModel.findOne({
+    _id: Sid,
+    classId: findClass._id,
+    roll: 'student'
+  });
+  classModel.updateOne(
+    {
+      _id: findClass._id,
+      'tests.subject': subject
+    },
+    {
+      $push: {
+        'tests.$.score': {
+          studentId: findStudent?._id,
+          score: newScore
+        }
+      }
+    }
+  );
+};
 
-// export const getAvarage = async (Tid: string): Promise<Number> => {
-//   const grades = await gradsModel.find({ teacherId: Tid });
-//   if (!grades) {
-//     throw new Error('No grades found for this teacher');
-//   }
-//   const ave = await gradsModel.aggregate([
-//     {
-//       $match: { teacherId: Tid }
-//     },
-//     {
-//       $group: {
-//         _id: '$subject',
-//         average: { $avg: '$average' }
-//       }
-//     }
-//   ]);
-//   return ave[0].average;
-// };
-// export const getStudentGradeByStudent = async (Sid: string) => {
-//   const grades = await gradsModel.find({ studentId: Sid });
-//   if (!grades) {
-//     throw new Error('No grades found for this student');
-//   }
-//   return grades;
-// };
-// export const getStudentGradeByIsTeacher = async (Tid: string) => {
-//   const grades = await gradsModel.find({ studentId: Tid });
-//   if (!grades) {
-//     throw new Error('No grades found for this student');
-//   }
-//   return grades;
-// };
-// export const updataGrade = async (Tid: string) => {
-//   const grades = await gradsModel.findByIdAndUpdate(Tid);
-// };
+export const updateScore = async (
+  Tid: mongoose.Types.ObjectId,
+  newScore: number,
+  Sid: string,
+  subject: string
+): Promise<void> => {
+  //find teacher
+  const findTeacher = await userModel.findById(Tid);
+  if (!findTeacher || findTeacher.roll !== 'teacher') {
+    throw new Error('you are not a teacher');
+  }
+  const findClass = await classModel.findById(Tid);
+  if (!findClass) {
+    throw new Error('no class found');
+  }
+  const findStudent = await userModel.findOne({
+    _id: Sid,
+    classId: findClass._id,
+    roll: 'student'
+  });
+  if (!findStudent) {
+    throw new Error('Student not found in this class');
+  }
+  classModel.updateOne(
+    {
+      _id: findClass._id,
+      'tests.subject': subject
+    },
+    {
+      $set: {
+        'tests.$.grades.$[grade].score': newScore
+      }
+    }
+  );
+};
+
+export const getAvarage = async (
+  Tid: mongoose.Types.ObjectId
+): Promise<void> => {
+  const findTeacher = await userModel.findById(Tid);
+  if (!findTeacher || findTeacher.roll !== 'teacher') {
+    throw new Error('you are not a teacher');
+  }
+  // const findClass = await classModel.findById(Tid);
+  // if (!findClass) {
+  //   throw new Error('no class found');
+  // }
+  classModel.aggregate([
+    { $match: Tid },
+    { $unwind: '$tests' },
+    {
+      $group: {
+        _id: null,
+        aveScore: { $avg: '$tests.score' }
+      }
+    }
+  ]);
+};
+export const getStudentGradeByStudent = async (
+  Sid: mongoose.Types.ObjectId
+): Promise<void> => {
+  const findStudent = await userModel.findById(Sid);
+  if (!findStudent || findStudent.roll !== 'student') {
+    throw new Error('you are not a student');
+  }
+
+  const scores = await classModel.aggregate([
+    { $unwind: '$tests' },
+
+    { $match: { 'tests.studentId': Sid } },
+
+    {
+      $group: {
+        _id: {
+          subject: '$tests.subject',
+          testId: '$tests._id'
+        },
+        score: { $first: '$tests.score' } // קח את הציון של המבחן
+      }
+    }
+  ]);
+
+  if (!scores.length) {
+    throw new Error('No scores found for this student.');
+  }
+};
+
+export const getStudentGradeByTeacher = async (
+  Tid: mongoose.Types.ObjectId,
+  Sid: string
+): Promise<void> => {
+  //find teacher
+  const findTeacher = await userModel.findById(Tid);
+  if (!findTeacher || findTeacher.roll !== 'teacher') {
+    throw new Error('you are not a teacher');
+  }
+
+  const scores = await classModel.aggregate([
+    { $unwind: '$tests' },
+
+    { $match: { 'tests.studentId': Sid } },
+
+    {
+      $group: {
+        _id: {
+          subject: '$tests.subject',
+          testId: '$tests._id'
+        },
+        score: { $first: '$tests.score' } 
+      }
+    }
+  ]);
+
+  if (!scores.length) {
+    throw new Error('No scores found for this student.');
+  }
+};
